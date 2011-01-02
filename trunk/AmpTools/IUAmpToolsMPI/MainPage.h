e09885822cc43b7161c6fc289577bb3c5d5914e9
mashephe <mashephe@ec4fb13d-7405-48cf-81e8-0bda2a8a6eb0>
2011-01-02 18:57:37 +0000

    added documentation

---
+++ trunk/AmpTools/IUAmpToolsMPI/MainPage.h
@@ -0,0 +1,16 @@
+
+/**
+ *\defgroup IUAmpToolsMPI
+ *
+ * The IUAmpToolsMPI directory contains classes for parallel-processing
+ * implementations of IUAmpTools using the Message Passing Interface (MPI).
+ * These classes allow fits to executed in parallel on multiple CPUs.  The
+ * classes enable a parallel computation of both the likelihood and associated
+ * normalization integrals.  In general the classes inherit from their 
+ * counterparts in IUAmpTools.  The design goal was to only implement 
+ * MPI transactions in these classes -- this avoids replication of key
+ * functions of the IUAmpTools framework.  Where possible the constructors
+ * to these clases were kept the same as IUAmpTools.  This should facilitate
+ * an easy switch between single process and MPI implementations.
+ *
+ */
--- trunk/AmpTools/MainPage.h
+++ trunk/AmpTools/MainPage.h
@@ -2,7 +2,9 @@
 /**
  * \mainpage AmpTools Class Reference
  *
- * This site contains a complete doxygen-generated source code reference for
- * the AmpTools package.
+ * This site contains an evolving doxygen-generated reference for the
+ * AmpTools package.  As additional doxygen documentation is added,
+ * this site will be continually updated.
+ * 
  */
 
