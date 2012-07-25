cfb3b5316a1ef87a4cb2be10da8db564465eda9b
remitche <remitche@ec4fb13d-7405-48cf-81e8-0bda2a8a6eb0>
2012-07-25 21:25:17 +0000

    Modified the BreitWigner class to reflect changes in the Amplitude class.

--- Tutorials/Dalitz/DalitzAmp/BreitWigner.h
+++ Tutorials/Dalitz/DalitzAmp/BreitWigner.h
@@ -13,7 +13,7 @@
 #ifdef GPU_ACCELERATION
 
 void GPUBreitWigner_exec( dim3 dimGrid, dim3 dimBlock, GPU_AMP_PROTO,
-                          GDouble mass0, GDouble width0,
+                          GDouble mass, GDouble width,
                           int daughter1, int daughter2 );
 
 #endif // GPU_ACCELERATION
@@ -27,11 +27,9 @@ class BreitWigner : public Amplitude{
 
 public:
 
-  BreitWigner() : Amplitude() { setDefaultStatus( true ); }
+  BreitWigner() : Amplitude() { }
 
-  BreitWigner( const AmpParameter& mass0, 
-               const AmpParameter& width0, 
-               int daughter1, int daughter2 ); 
+  BreitWigner( const vector< string >& args );
 
   ~BreitWigner(){}
 
@@ -53,8 +51,8 @@ public:
   
 private:
 	
-  AmpParameter m_mass0;
-  AmpParameter m_width0;
+  AmpParameter m_mass;
+  AmpParameter m_width;
   int m_daughter1;
   int m_daughter2;  
 
