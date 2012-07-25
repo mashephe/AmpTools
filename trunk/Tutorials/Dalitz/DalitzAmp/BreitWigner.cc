1f50aac84a3e4aa618b5800ead328395ad8d2dac
remitche <remitche@ec4fb13d-7405-48cf-81e8-0bda2a8a6eb0>
2012-07-25 21:25:17 +0000

    Modified the BreitWigner class to reflect changes in the Amplitude class.

--- trunk/Tutorials/Dalitz/DalitzAmp/BreitWigner.cc
+++ trunk/Tutorials/Dalitz/DalitzAmp/BreitWigner.cc
@@ -7,23 +7,21 @@
 #include "IUAmpTools/Kinematics.h"
 #include "DalitzAmp/BreitWigner.h"
 
-BreitWigner::BreitWigner( const AmpParameter& mass0, 
-                          const AmpParameter& width0,
-                          int daughter1, int daughter2) :
-Amplitude(),
-m_mass0( mass0 ),
-m_width0( width0 ),
-m_daughter1(daughter1),
-m_daughter2(daughter2)
+BreitWigner::BreitWigner( const vector< string >& args ) :
+Amplitude(args)
 {
-  
-  // this is not the default constructor
-  setDefaultStatus( false );
+
+  assert( args.size() == 4 );
+
+  m_mass = AmpParameter(args[0]);
+  m_width = AmpParameter(args[1]);
+  m_daughter1 = atoi(args[2].c_str());
+  m_daughter2 = atoi(args[3].c_str());
   
   // need to register any free parameters so the framework knows about them
-  registerParameter( m_mass0 );
-  registerParameter( m_width0 );
-  
+  registerParameter( m_mass );
+  registerParameter( m_width );
+
 }
 
 
@@ -38,35 +36,12 @@ BreitWigner::calcAmplitude( GDouble** pKin ) const {
 
   GDouble m = (P1+P2).m();
 
-  complex<GDouble> bwdenominator(m*m - m_mass0*m_mass0, m_mass0*m_width0);
+  complex<GDouble> bwdenominator(m*m - m_mass*m_mass, m_mass*m_width);
 
   return  complex<GDouble>(1.0,0.0) / bwdenominator;
 
 }
 
-
-BreitWigner*
-BreitWigner::newAmplitude( const vector< string >& args ) const {
-
-  assert( args.size() == 4 );
-
-  AmpParameter mass(args[0]);
-  AmpParameter width(args[1]);
-  int daughter1(atoi(args[2].c_str()));
-  int daughter2(atoi(args[3].c_str()));
-
-  return new BreitWigner( mass, width, daughter1, daughter2 );
-
-}
-
-
-BreitWigner*
-BreitWigner::clone() const {
-
-  return ( isDefault() ? new BreitWigner() : 
-    new BreitWigner( m_mass0, m_width0, m_daughter1, m_daughter2 ) );
-
-}
 
 
 #ifdef GPU_ACCELERATION
@@ -77,8 +52,20 @@ BreitWigner::launchGPUKernel( dim3 dimGrid, dim3 dimBlock, GPU_AMP_PROTO ) const
   // decimal place
   
   GPUBreitWigner_exec( dimGrid,  dimBlock, GPU_AMP_ARGS, 
-                       m_mass0, m_width0, m_daughter1, m_daughter2 );
+                       m_mass, m_width, m_daughter1, m_daughter2 );
 
 }
 #endif //GPU_ACCELERATION
+
+
+BreitWigner*
+BreitWigner::newAmplitude( const vector< string >& args ) const {
+  return new BreitWigner( args );
+}
+
+BreitWigner*
+BreitWigner::clone() const {
+  return ( isDefault() ? new BreitWigner() : 
+    new BreitWigner( arguments() ) );
+}
 
