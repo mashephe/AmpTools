1ceaff83cc2b5e907223180c2289a4786ba289b7
Matthew Shepherd <mashephe@users.noreply.github.com>
2012-11-16 02:20:32 +0000

    fix plot generator to use new base class

--- Tutorials/gammaKK/gammaKKPlot/gammaKKPlotGenerator.h
+++ Tutorials/gammaKK/gammaKKPlot/gammaKKPlotGenerator.h
@@ -1,14 +1,10 @@
 #if !(defined GAMMAKKPLOTGENERATOR)
 #define GAMMAKKPLOTGENERATOR
 
-#include <vector>
-#include <string>
-#include "IUAmpTools/ConfigurationInfo.h"
 #include "IUAmpTools/PlotGenerator.h"
-#include "IUAmpTools/Histogram.h"
-#include "IUAmpTools/AmplitudeManager.h"
-#include "gammaKKDataIO/gammaKKDataReader.h"
 
+class AmpToolsInterface;
+class Kinematics;
 
 using namespace std;
 
@@ -17,39 +13,20 @@ class gammaKKPlotGenerator : public PlotGenerator
     
 public:
     
-  gammaKKPlotGenerator( const ConfigurationInfo* cfgInfo,
-                       const string& parFile );
+  gammaKKPlotGenerator( AmpToolsInterface& ati );
 
-  virtual ~gammaKKPlotGenerator();
-
-  const vector< string >& availablePlots() const;
-
-  enum HistIndex { 
+  enum { 
     khm12 = 0, khm13, khm23,
     // angles of photon in CM frame
     khPhotonCosTheta, khPhotonPhi,
     // angles of 1st K in KK CM frame
     khKaonCosTheta, khKaonPhi,
-    kNumHist 
+    kNumHists
   };
     
 private:
         
-  vector< Histogram > fillProjections( const string& fsName,
-                                          PlotType type );
-
-  void registerPhysics( AmplitudeManager* ampManager );
-
-  vector< string > m_histTitles;
-
-  mutable vector< Histogram > m_histVect;
-
-  void clearHistograms();
-  void bookHistograms();
-
-  map< string, AmpVecs > m_dataMap;
-  map< string, AmpVecs > m_accMCMap;
-  map< string, AmpVecs > m_genMCMap;
+  void projectEvent( Kinematics* kin );
 
 };
 
