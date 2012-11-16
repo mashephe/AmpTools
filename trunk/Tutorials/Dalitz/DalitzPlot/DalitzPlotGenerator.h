c3a79a08e6b12d68c6ef3fa195abc76c48537662
mashephe <mashephe@ec4fb13d-7405-48cf-81e8-0bda2a8a6eb0>
2012-11-16 00:11:10 +0000

    revise and simplify class for new PlotGenerator that uses the AmpToolsInterface

--- trunk/Tutorials/Dalitz/DalitzPlot/DalitzPlotGenerator.h
+++ trunk/Tutorials/Dalitz/DalitzPlot/DalitzPlotGenerator.h
@@ -1,51 +1,28 @@
 #if !(defined DALITZPLOTGENERATOR)
 #define DALITZPLOTGENERATOR
 
-#include <vector>
-#include <string>
-#include "IUAmpTools/ConfigurationInfo.h"
-#include "IUAmpTools/PlotGenerator.h"
-#include "IUAmpTools/Histogram.h"
-#include "IUAmpTools/AmplitudeManager.h"
-#include "DalitzDataIO/DalitzDataReader.h"
-
-
 using namespace std;
+
+#include "IUAmpTools/PlotGenerator.h"
+
+class AmpToolsInterface;
+class Kinematics;
 
 class DalitzPlotGenerator : public PlotGenerator
 {
     
 public:
     
-  DalitzPlotGenerator( const ConfigurationInfo* cfgInfo,
-                       const string& parFile );
+  DalitzPlotGenerator( AmpToolsInterface& ati );
 
-  virtual ~DalitzPlotGenerator();
-
-  const vector< string >& availablePlots() const;
-
-  enum HistIndex { 
+  enum { 
     khm12 = 0, khm13, khm23, 
-    kNumHist 
+    kNumHists 
   };
     
 private:
         
-  vector< Histogram > fillProjections( const string& fsName,
-                                          PlotType type );
-
-  void registerPhysics( AmplitudeManager* ampManager );
-
-  vector< string > m_histTitles;
-
-  mutable vector< Histogram > m_histVect;
-
-  void clearHistograms();
-  void bookHistograms();
-
-  map< string, AmpVecs > m_dataMap;
-  map< string, AmpVecs > m_accMCMap;
-  map< string, AmpVecs > m_genMCMap;
+  void projectEvent( Kinematics* kin );
 
 };
 
