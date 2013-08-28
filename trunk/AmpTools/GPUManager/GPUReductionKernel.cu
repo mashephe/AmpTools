875025ae612e9f3feddd302150743d3cd066b653
mashephe <mashephe@ec4fb13d-7405-48cf-81e8-0bda2a8a6eb0>
2013-08-28 01:40:48 +0000

    remove unused reduction kernels

--- trunk/AmpTools/GPUManager/GPUReductionKernel.cu
+++ trunk/AmpTools/GPUManager/GPUReductionKernel.cu
@@ -133,246 +133,6 @@ struct SharedMemory<double>
     - only works for power-of-2 arrays
 */
 
-/* This reduction interleaves which threads are active by using the modulo
-   operator.  This operator is very expensive on GPUs, and the interleaved 
-   inactivity means that no whole warps are active, which is also very 
-   inefficient */
-template <class T>
-__global__ void
-reduce0(T *g_idata, T *g_odata, unsigned int n)
-{
-    T *sdata = SharedMemory<T>();
-
-    // load shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-    
-    sdata[tid] = (i < n) ? g_idata[i] : 0;
-    
-    __syncthreads();
-
-    // do reduction in shared mem
-    for(unsigned int s=1; s < blockDim.x; s *= 2) {
-        // modulo arithmetic is slow!
-        if ((tid % (2*s)) == 0) {
-            sdata[tid] += sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
-}
-
-/* This version uses contiguous threads, but its interleaved 
-   addressing results in many shared memory bank conflicts. 
-*/
-template <class T>
-__global__ void
-reduce1(T *g_idata, T *g_odata, unsigned int n)
-{
-    T *sdata = SharedMemory<T>();
-
-    // load shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-    
-    sdata[tid] = (i < n) ? g_idata[i] : 0;
-    
-    __syncthreads();
-
-    // do reduction in shared mem
-    for(unsigned int s=1; s < blockDim.x; s *= 2) 
-    {
-        int index = 2 * s * tid;
-
-        if (index < blockDim.x) 
-        {
-            sdata[index] += sdata[index + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
-}
-
-/*
-    This version uses sequential addressing -- no divergence or bank conflicts.
-*/
-template <class T>
-__global__ void
-reduce2(T *g_idata, T *g_odata, unsigned int n)
-{
-    T *sdata = SharedMemory<T>();
-
-    // load shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-    
-    sdata[tid] = (i < n) ? g_idata[i] : 0;
-    
-    __syncthreads();
-
-    // do reduction in shared mem
-    for(unsigned int s=blockDim.x/2; s>0; s>>=1) 
-    {
-        if (tid < s) 
-        {
-            sdata[tid] += sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem
-    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
-}
-
-/*
-    This version uses n/2 threads --
-    it performs the first level of reduction when reading from global memory.
-*/
-template <class T>
-__global__ void
-reduce3(T *g_idata, T *g_odata, unsigned int n)
-{
-    T *sdata = SharedMemory<T>();
-
-    // perform first level of reduction,
-    // reading from global memory, writing to shared memory
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*(blockDim.x*2) + threadIdx.x;
-
-    T mySum = (i < n) ? g_idata[i] : 0;
-    if (i + blockDim.x < n) 
-        mySum += g_idata[i+blockDim.x];  
-
-    sdata[tid] = mySum;
-    __syncthreads();
-
-    // do reduction in shared mem
-    for(unsigned int s=blockDim.x/2; s>0; s>>=1) 
-    {
-        if (tid < s) 
-        {
-            sdata[tid] = mySum = mySum + sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-    // write result for this block to global mem 
-    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
-}
-
-/*
-    This version unrolls the last warp to avoid synchronization where it 
-    isn't needed.
-
-    Note, this kernel needs a minimum of 64*sizeof(T) bytes of shared memory. 
-    In other words if blockSize <= 32, allocate 64*sizeof(T) bytes.  
-    If blockSize > 32, allocate blockSize*sizeof(T) bytes.
-*/
-template <class T, unsigned int blockSize>
-__global__ void
-reduce4(T *g_idata, T *g_odata, unsigned int n)
-{
-    T *sdata = SharedMemory<T>();
-
-    // perform first level of reduction,
-    // reading from global memory, writing to shared memory
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*(blockDim.x*2) + threadIdx.x;
-
-    T mySum = (i < n) ? g_idata[i] : 0;
-    if (i + blockSize < n) 
-        mySum += g_idata[i+blockSize];  
-
-    sdata[tid] = mySum;
-    __syncthreads();
-
-    // do reduction in shared mem
-    for(unsigned int s=blockDim.x/2; s>32; s>>=1) 
-    {
-        if (tid < s)
-        {
-            sdata[tid] = mySum = mySum + sdata[tid + s];
-        }
-        __syncthreads();
-    }
-
-#ifndef __DEVICE_EMULATION__
-    if (tid < 32)
-#endif
-    {
-        // now that we are using warp-synchronous programming (below)
-        // we need to declare our shared memory volatile so that the compiler
-        // doesn't reorder stores to it and induce incorrect behavior.
-        volatile T *smem = sdata;
-        if (blockSize >=  64) { smem[tid] = mySum = mySum + smem[tid + 32]; EMUSYNC; }
-        if (blockSize >=  32) { smem[tid] = mySum = mySum + smem[tid + 16]; EMUSYNC; }
-        if (blockSize >=  16) { smem[tid] = mySum = mySum + smem[tid +  8]; EMUSYNC; }
-        if (blockSize >=   8) { smem[tid] = mySum = mySum + smem[tid +  4]; EMUSYNC; }
-        if (blockSize >=   4) { smem[tid] = mySum = mySum + smem[tid +  2]; EMUSYNC; }
-        if (blockSize >=   2) { smem[tid] = mySum = mySum + smem[tid +  1]; EMUSYNC; }
-    }
-
-    // write result for this block to global mem 
-    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
-}
-
-/*
-    This version is completely unrolled.  It uses a template parameter to achieve 
-    optimal code for any (power of 2) number of threads.  This requires a switch 
-    statement in the host code to handle all the different thread block sizes at 
-    compile time.
-
-    Note, this kernel needs a minimum of 64*sizeof(T) bytes of shared memory. 
-    In other words if blockSize <= 32, allocate 64*sizeof(T) bytes.  
-    If blockSize > 32, allocate blockSize*sizeof(T) bytes.
-*/
-template <class T, unsigned int blockSize>
-__global__ void
-reduce5(T *g_idata, T *g_odata, unsigned int n)
-{
-    T *sdata = SharedMemory<T>();
-
-    // perform first level of reduction,
-    // reading from global memory, writing to shared memory
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*(blockSize*2) + threadIdx.x;
-
-    T mySum = (i < n) ? g_idata[i] : 0;
-    if (i + blockSize < n) 
-        mySum += g_idata[i+blockSize];  
-
-    sdata[tid] = mySum;
-    __syncthreads();
-
-    // do reduction in shared mem
-    if (blockSize >= 512) { if (tid < 256) { sdata[tid] = mySum = mySum + sdata[tid + 256]; } __syncthreads(); }
-    if (blockSize >= 256) { if (tid < 128) { sdata[tid] = mySum = mySum + sdata[tid + 128]; } __syncthreads(); }
-    if (blockSize >= 128) { if (tid <  64) { sdata[tid] = mySum = mySum + sdata[tid +  64]; } __syncthreads(); }
-    
-#ifndef __DEVICE_EMULATION__
-    if (tid < 32)
-#endif
-    {
-        // now that we are using warp-synchronous programming (below)
-        // we need to declare our shared memory volatile so that the compiler
-        // doesn't reorder stores to it and induce incorrect behavior.
-        volatile T* smem = sdata;
-        if (blockSize >=  64) { smem[tid] = mySum = mySum + smem[tid + 32]; EMUSYNC; }
-        if (blockSize >=  32) { smem[tid] = mySum = mySum + smem[tid + 16]; EMUSYNC; }
-        if (blockSize >=  16) { smem[tid] = mySum = mySum + smem[tid +  8]; EMUSYNC; }
-        if (blockSize >=   8) { smem[tid] = mySum = mySum + smem[tid +  4]; EMUSYNC; }
-        if (blockSize >=   4) { smem[tid] = mySum = mySum + smem[tid +  2]; EMUSYNC; }
-        if (blockSize >=   2) { smem[tid] = mySum = mySum + smem[tid +  1]; EMUSYNC; }
-    }
-    
-    // write result for this block to global mem 
-    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
-}
-
 /*
     This version adds multiple elements per thread sequentially.  This reduces the overall
     cost of the algorithm while keeping the work complexity O(n) and the step complexity O(log n).
@@ -412,11 +172,11 @@ reduce6(T *g_idata, T *g_odata, unsigned int n)
     sdata[tid] = mySum;
     __syncthreads();
 
-
     // do reduction in shared mem
-    if (blockSize >= 512) { if (tid < 256) { sdata[tid] = mySum = mySum + sdata[tid + 256]; } __syncthreads(); }
-    if (blockSize >= 256) { if (tid < 128) { sdata[tid] = mySum = mySum + sdata[tid + 128]; } __syncthreads(); }
-    if (blockSize >= 128) { if (tid <  64) { sdata[tid] = mySum = mySum + sdata[tid +  64]; } __syncthreads(); }
+    if (blockSize >= 1024) { if (tid < 512) { sdata[tid] = mySum = mySum + sdata[tid + 512]; } __syncthreads(); }
+    if (blockSize >= 512 ) { if (tid < 256) { sdata[tid] = mySum = mySum + sdata[tid + 256]; } __syncthreads(); }
+    if (blockSize >= 256 ) { if (tid < 128) { sdata[tid] = mySum = mySum + sdata[tid + 128]; } __syncthreads(); }
+    if (blockSize >= 128 ) { if (tid <  64) { sdata[tid] = mySum = mySum + sdata[tid +  64]; } __syncthreads(); }
     
 #ifndef __DEVICE_EMULATION__
     if (tid < 32)
@@ -440,17 +200,13 @@ reduce6(T *g_idata, T *g_odata, unsigned int n)
 }
 
 
-extern "C"
-bool isPow2(unsigned int x);
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // Wrapper function for kernel launch
 ////////////////////////////////////////////////////////////////////////////////
 template <class T>
 void 
 reduce(int size, int threads, int blocks, 
-       int whichKernel, T *d_idata, T *d_odata)
+       T *d_idata, T *d_odata)
 {
     dim3 dimBlock(threads, 1, 1);
     dim3 dimGrid(blocks, 1, 1);
@@ -459,142 +215,45 @@ reduce(int size, int threads, int blocks,
     // worth of shared memory so that we don't index shared memory out of bounds
     int smemSize = (threads <= 32) ? 2 * threads * sizeof(T) : threads * sizeof(T);
 
-    // choose which of the optimized versions of reduction to launch
-    switch (whichKernel)
+    switch (threads)
     {
-    case 0:
-        reduce0<T><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size);
-        break;
-    case 1:
-        reduce1<T><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size);
-        break;
-    case 2:
-        reduce2<T><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size);
-        break;
-    case 3:
-        reduce3<T><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size);
-        break;
-    case 4:
-        switch (threads)
-        {
-        case 512:
-            reduce4<T, 512><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 256:
-            reduce4<T, 256><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 128:
-            reduce4<T, 128><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 64:
-            reduce4<T,  64><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 32:
-            reduce4<T,  32><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 16:
-            reduce4<T,  16><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case  8:
-            reduce4<T,   8><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case  4:
-            reduce4<T,   4><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case  2:
-            reduce4<T,   2><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case  1:
-            reduce4<T,   1><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        }
-        break; 
-    case 5:
-        switch (threads)
-        {
-        case 512:
-            reduce5<T, 512><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 256:
-            reduce5<T, 256><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 128:
-            reduce5<T, 128><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 64:
-            reduce5<T,  64><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 32:
-            reduce5<T,  32><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case 16:
-            reduce5<T,  16><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case  8:
-            reduce5<T,   8><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case  4:
-            reduce5<T,   4><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case  2:
-            reduce5<T,   2><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        case  1:
-            reduce5<T,   1><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-        }
-        break;       
-    case 6:
-    default:
-//        if (isPow2(size))  -- MS note: code guarantees array has length of a power of 2
-//        {
-            switch (threads)
-            {
-            case 512:
-                reduce6<T, 512, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 256:
-                reduce6<T, 256, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 128:
-                reduce6<T, 128, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 64:
-                reduce6<T,  64, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 32:
-                reduce6<T,  32, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 16:
-                reduce6<T,  16, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case  8:
-                reduce6<T,   8, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case  4:
-                reduce6<T,   4, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case  2:
-                reduce6<T,   2, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case  1:
-                reduce6<T,   1, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            }
-/*
-        }
-        else
-        {
-            switch (threads)
-            {
-            case 512:
-                reduce6<T, 512, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 256:
-                reduce6<T, 256, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 128:
-                reduce6<T, 128, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 64:
-                reduce6<T,  64, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 32:
-                reduce6<T,  32, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case 16:
-                reduce6<T,  16, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case  8:
-                reduce6<T,   8, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case  4:
-                reduce6<T,   4, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case  2:
-                reduce6<T,   2, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            case  1:
-                reduce6<T,   1, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
-            }
-        }
-*/
-        break;       
+      case 1024:
+          reduce6<T, 1024, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case 512:
+          reduce6<T, 512, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case 256:
+           reduce6<T, 256, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case 128:
+           reduce6<T, 128, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case 64:
+          reduce6<T,  64, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case 32:
+          reduce6<T,  32, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case 16:
+          reduce6<T,  16, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case  8:
+          reduce6<T,   8, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case  4:
+          reduce6<T,   4, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case  2:
+          reduce6<T,   2, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      case  1:
+          reduce6<T,   1, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+
     }
 }
 
 // Instantiate the reduction function for 3 types
 template void 
 reduce<int>(int size, int threads, int blocks, 
-            int whichKernel, int *d_idata, int *d_odata);
+            int *d_idata, int *d_odata);
 
 template void 
 reduce<float>(int size, int threads, int blocks, 
-              int whichKernel, float *d_idata, float *d_odata);
+              float *d_idata, float *d_odata);
 
 template void 
 reduce<double>(int size, int threads, int blocks, 
-               int whichKernel, double *d_idata, double *d_odata);
+               double *d_idata, double *d_odata);
 
 #endif // #ifndef _REDUCE_KERNEL_H_
