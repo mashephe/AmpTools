ee04e667e86680f7e39f3085439accb17b1cfbd2
Matthew Shepherd <mashephe@users.noreply.github.com>
2014-03-13 19:28:45 +0000

    new kernel to assemble factors and permutations into amplitudes on the GPU

---
+++ AmpTools/GPUManager/GPUFactPermKernel.cu
@@ -0,0 +1,78 @@
+//******************************************************************************
+// This file is part of AmpTools, a package for performing Amplitude Analysis
+// 
+// Copyright Trustees of Indiana University 2010, all rights reserved
+// 
+// This software written by Matthew Shepherd, Ryan Mitchell, and 
+//                  Hrayr Matevosyan at Indiana University, Bloomington
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice and author attribution, this list of conditions and the
+//    following disclaimer. 
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice and author attribution, this list of conditions and the
+//    following disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+// 3. Neither the name of the University nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// 
+// Creation of derivative forms of this software for commercial
+// utilization may be subject to restriction; written permission may be
+// obtained from the Trustees of Indiana University.
+// 
+// INDIANA UNIVERSITY AND THE AUTHORS MAKE NO REPRESENTATIONS OR WARRANTIES, 
+// EXPRESS OR IMPLIED.  By way of example, but not limitation, INDIANA 
+// UNIVERSITY MAKES NO REPRESENTATIONS OR WARRANTIES OF MERCANTABILITY OR 
+// FITNESS FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR 
+// DOCUMENTATION WILL NOT INFRINGE ANY PATENTS, COPYRIGHTS, TRADEMARKS, 
+// OR OTHER RIGHTS.  Neither Indiana University nor the authors shall be 
+// held liable for any liability with respect to any claim by the user or 
+// any other party arising from use of the program.
+//******************************************************************************
+
+#include "GPUCustomTypes.h"
+
+__global__ void
+fact_perm_kernel( GDouble* pfDevAmps, GDouble* pcDevCalcAmp, int nFact,
+                  int nPerm, int nEvents )
+{
+
+	int i = threadIdx.x + GPU_BLOCK_SIZE_X * threadIdx.y + 
+            ( blockIdx.x + blockIdx.y * gridDim.x ) * GPU_BLOCK_SIZE_SQ;
+
+  for( int iPerm = 0; iPerm < nPerm; ++iPerm ){
+  
+    int offsetP = 2*nEvents*iPerm + 2*i;
+    
+    double ampRe = pcDevCalcAmp[offsetP];
+    double ampIm = pcDevCalcAmp[offsetP+1];
+
+    for( int iFactor = 1; iFactor < nFact; ++iFactor ){
+    
+       int offsetF = 2*nEvents*nPerm*iFactor + offsetP;
+
+       double re = ampRe;
+       double im = ampIm;
+       
+       ampRe = re * pcDevCalcAmp[offsetF] - im * pcDevCalcAmp[offsetF+1];
+       ampIm = re * pcDevCalcAmp[offsetF+1] + im * pcDevCalcAmp[offsetF];
+    }
+    
+    pfDevAmps[2*i]   += ampRe;
+    pfDevAmps[2*i+1] += ampIm;
+  }
+
+  pfDevAmps[2*i]   /= G_SQRT( nPerm*1.0 );
+  pfDevAmps[2*i+1] /= G_SQRT( nPerm*1.0 );
+}
+
+extern "C" void GPU_ExecFactPermKernel( dim3 dimGrid, dim3 dimBlock,
+    GDouble* pfDevAmps, GDouble* pcDevCalcAmp, int nFact, int nPerm, int nEvents )
+{
+  fact_perm_kernel<<< dimGrid, dimBlock >>>( pfDevAmps, pcDevCalcAmp, 
+                                             nFact, nPerm, nEvents );
+}
