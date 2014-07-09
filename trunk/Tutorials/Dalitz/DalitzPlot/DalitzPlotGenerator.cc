a846d75ea269a201af9e7a801b0a51d38bc0e060
mashephe <mashephe@ec4fb13d-7405-48cf-81e8-0bda2a8a6eb0>
2014-07-09 18:46:31 +0000

    include example 2D plot

--- trunk/Tutorials/Dalitz/DalitzPlot/DalitzPlotGenerator.cc
+++ trunk/Tutorials/Dalitz/DalitzPlot/DalitzPlotGenerator.cc
@@ -9,6 +9,7 @@ PlotGenerator( results )
   bookHistogram( khm12, "Mass( 1 2 )", new Histogram1D( 60, 0.0, 3.0 ) );
   bookHistogram( khm13, "Mass( 1 3 )", new Histogram1D( 60, 0.0, 3.0 ) );
   bookHistogram( khm23, "Mass( 2 3 )", new Histogram1D( 60, 0.0, 3.0 ) );
+  bookHistogram( kdltz, "Dalitz Plot", new Histogram2D( 60, 0.0, 9.0, 60, 0.0, 9.0 ) );
 }
 
 void
@@ -20,5 +21,6 @@ DalitzPlotGenerator::projectEvent( Kinematics* kin ){
       
   fillHistogram( khm12, (P1+P2).m() );
   fillHistogram( khm13, (P1+P3).m() );  
-  fillHistogram( khm23, (P2+P3).m() );  
+  fillHistogram( khm23, (P2+P3).m() );
+  fillHistogram( kdltz, (P1+P2).m2(), (P2+P3).m2() );
 }
