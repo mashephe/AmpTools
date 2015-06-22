3afe02645631a2d435093d095a1555042ff58f4f
mashephe <mashephe@ec4fb13d-7405-48cf-81e8-0bda2a8a6eb0>
2015-06-22 19:52:01 +0000

    demonstrate new style of using user-defined histogram names and titles

--- trunk/Tutorials/Dalitz/DalitzPlot/DalitzPlotGenerator.cc
+++ trunk/Tutorials/Dalitz/DalitzPlot/DalitzPlotGenerator.cc
@@ -7,10 +7,10 @@
 DalitzPlotGenerator::DalitzPlotGenerator( const FitResults& results ) :
 PlotGenerator( results )
 {
-  bookHistogram( khm12, "Mass( 1 2 )", new Histogram1D( 60, 0.0, 3.0 ) );
-  bookHistogram( khm13, "Mass( 1 3 )", new Histogram1D( 60, 0.0, 3.0 ) );
-  bookHistogram( khm23, "Mass( 2 3 )", new Histogram1D( 60, 0.0, 3.0 ) );
-  bookHistogram( kdltz, "Dalitz Plot", new Histogram2D( 60, 0.0, 9.0, 60, 0.0, 9.0 ) );
+  bookHistogram( khm12, new Histogram1D( 60, 0.0, 3.0, "hm12", "Mass( 1 2 )" ) );
+  bookHistogram( khm13, new Histogram1D( 60, 0.0, 3.0, "hm13", "Mass( 1 3 )" ) );
+  bookHistogram( khm23, new Histogram1D( 60, 0.0, 3.0, "hm23", "Mass( 2 3 )" ) );
+  bookHistogram( kdltz, new Histogram2D( 60, 0.0, 9.0, 60, 0.0, 9.0, "dltz", "Dalitz Plot" ) );
 }
 
 void
