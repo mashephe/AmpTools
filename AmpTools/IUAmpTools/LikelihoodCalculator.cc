1b5093d49488e4b345a43153198017f4594b51ac
Matthew Shepherd <mashephe@indiana.edu>
2021-01-10 05:32:52 -0500

    remove tracking of the sum of data weights, which is not used in the calculation any longer; print a variety of warning and notification messages to the screen when different cases of weighted events are detected

--- AmpTools/IUAmpTools/LikelihoodCalculator.cc
+++ AmpTools/IUAmpTools/LikelihoodCalculator.cc
@@ -67,7 +67,6 @@ m_firstDataCalc( true ),
 m_firstNormIntCalc( true ),
 m_sumBkgWeights( 0 ),
 m_numBkgEvents( 0 ),
-m_sumDataWeights( 0 ),
 m_numDataEvents( 0 )
 {
   
@@ -184,8 +183,8 @@ LikelihoodCalculator::normIntTerm(){
     // this is the number of predicted signal and background events
     double nPred = normTerm + m_sumBkgWeights;
 
-    normTerm = ( m_sumDataWeights - m_sumBkgWeights ) * log( normTerm );
-    normTerm += nPred - ( m_sumDataWeights * log( nPred ) );
+    normTerm = ( m_numDataEvents - m_sumBkgWeights ) * log( normTerm );
+    normTerm += nPred - ( m_numDataEvents * log( nPred ) );
     
     return normTerm;
   }
@@ -216,15 +215,52 @@ LikelihoodCalculator::dataTerm(){
     m_ampVecsSignal.loadData( m_dataReaderSignal );
     m_ampVecsSignal.allocateTerms( m_intenManager, true );
 
-    m_sumDataWeights = m_ampVecsSignal.m_dSumWeights;
     m_numDataEvents = m_ampVecsSignal.m_iNTrueEvents;
     
-    if( m_hasBackground ){
+    if( m_ampVecsSignal.m_hasNonUnityWeights && m_hasBackground ){
     
-      m_ampVecsBkgnd.loadData( m_dataReaderBkgnd, true );
+      cout
+      << "***************************************************************\n"
+      << "* WARNING!: Events in the signal data sample have weights     *\n"
+      << "* that differ from 1 and a background data sample has been    *\n"
+      << "* provided.  This will produce incorrect results.  The        *\n"
+      << "* recommended solution is to set all signal weights to unity  *\n"
+      << "* and put all background events in the background file with   *\n"
+      << "* weights such that the weighted sum mimics the background    *\n"
+      << "* contribution to the likelihood.                             *\n"
+      << "***************************************************************\n" << endl;
+    }
+    
+    if( m_hasBackground ){
+          
+      m_ampVecsBkgnd.loadData( m_dataReaderBkgnd );
       m_ampVecsBkgnd.allocateTerms( m_intenManager, true );
 
+      if( m_ampVecsBkgnd.m_hasMixedSignWeights ){
+        cout
+        << "***************************************************************\n"
+        << "* NOTICE:  Weights with both positive and negative signs were *\n"
+        << "* detected in the background file.  This may be desirable for *\n"
+        << "* some applications.  Older versions of AmpTools (v0.10.x and *\n"
+        << "* prior) will not properly handle this case and will also not *\n"
+        << "* print this notification to the screen.                      *\n"
+        << "***************************************************************\n" << endl;
+      }
+      
       m_sumBkgWeights = m_ampVecsBkgnd.m_dSumWeights;
+      
+      if( m_sumBkgWeights < 0 ){
+        cout
+        << "***************************************************************\n"
+        << "* ERROR: The sum of all background weights is negative.  This *\n"
+        << "* implies a negative background in the signal region, which   *\n"
+        << "* unphysical.  The weighted sum of the background events      *\n"
+        << "* should represent the background contribution to the signal  *\n"
+        << "* region.                                                     *\n"
+        << "***************************************************************\n" << endl;
+        assert( false );
+      }
+      
       m_numBkgEvents = m_ampVecsBkgnd.m_iNTrueEvents;
     }
     
@@ -233,9 +269,13 @@ LikelihoodCalculator::dataTerm(){
   
   double sumLnI = m_intenManager.calcSumLogIntensity( m_ampVecsSignal );
   
+  // if there is a background file, we try to correct the sumLnI for background
+  // by subtracting the contribution to the likelihood as derived from the
+  // weighted background sample (the sign change here from v0.10.x and prior is
+  // consistent with no longer forcing weights in the background sample to be negative)
   if( m_hasBackground ){
 
-    sumLnI += m_intenManager.calcSumLogIntensity( m_ampVecsBkgnd );
+    sumLnI -= m_intenManager.calcSumLogIntensity( m_ampVecsBkgnd );
   }
   
   m_firstDataCalc = false;
