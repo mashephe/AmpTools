91f3e7d564203afbad83733517c50b9968886848
Matthew Shepherd <mashephe@indiana.edu>
2022-01-13 08:53:30 -0500

    replace function calls that were deprecated in MPI v2 and removed in v4

--- AmpTools/IUAmpToolsMPI/DataReaderMPI.h
+++ AmpTools/IUAmpToolsMPI/DataReaderMPI.h
@@ -6,21 +6,6 @@
 #include "IUAmpTools/Kinematics.h"
 #include "IUAmpToolsMPI/MPITag.h"
 
-// starting with MPI v4 there appears to be a
-// backwards incompatible change in the name of
-// some functions
-#if MPI_VERSION > 3
-
-#define MPI_ADDRESS(A,B) MPI_Get_address(A,B)
-#define MPI_TYPE_STRUCT(A,B,C,D,E) MPI_Type_create_struct(A,B,C,D,E)
-
-#else
-
-#define MPI_ADDRESS(A,B) MPI_Address(A,B)
-#define MPI_TYPE_STRUCT(A,B,C,D,E) MPI_Type_struct(A,B,C,D,E)
-
-#endif
-
 /**
  * This is a helper struct that is used to pass kinematic data between
  * different processes using MPI.
@@ -373,39 +358,39 @@ void DataReaderMPI<T>::defineMPIType()
   MPI_Datatype type[6];
   
   MPI_Aint baseAddress;
-  MPI_ADDRESS( &kinStruct, &baseAddress );
+  MPI_Get_address( &kinStruct, &baseAddress );
   
   length[0] = 1;
-  MPI_ADDRESS( &kinStruct.nPart, &loc[0] );
+  MPI_Get_address( &kinStruct.nPart, &loc[0] );
   loc[0] -= baseAddress;
   type[0] = MPI_INT;
   
   length[1] = 1;
-  MPI_ADDRESS( &kinStruct.weight, &loc[1] );
+  MPI_Get_address( &kinStruct.weight, &loc[1] );
   loc[1] -= baseAddress;
   type[1] = MPI_FLOAT;
   
   length[2] = Kinematics::kMaxParticles;
-  MPI_ADDRESS( &kinStruct.e, &loc[2] );
+  MPI_Get_address( &kinStruct.e, &loc[2] );
   loc[2] -= baseAddress;
   type[2] = MPI_FLOAT;
   
   length[3] = Kinematics::kMaxParticles;
-  MPI_ADDRESS( &kinStruct.px, &loc[3] );
+  MPI_Get_address( &kinStruct.px, &loc[3] );
   loc[3] -= baseAddress;
   type[3] = MPI_FLOAT;
   
   length[4] = Kinematics::kMaxParticles;
-  MPI_ADDRESS( &kinStruct.py, &loc[4] );
+  MPI_Get_address( &kinStruct.py, &loc[4] );
   loc[4] -= baseAddress;
   type[4] = MPI_FLOAT;
   
   length[5] = Kinematics::kMaxParticles;
-  MPI_ADDRESS( &kinStruct.pz, &loc[5] );
+  MPI_Get_address( &kinStruct.pz, &loc[5] );
   loc[5] -= baseAddress;
   type[5] = MPI_FLOAT;
   
-  MPI_TYPE_STRUCT( 6, length, loc, type, &MPI_KinStruct );
+  MPI_Type_create_struct( 6, length, loc, type, &MPI_KinStruct );
   MPI_Type_commit( &MPI_KinStruct );
 }
 
